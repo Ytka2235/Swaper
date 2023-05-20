ab2004a4d6875a5ce4339707f9814427e423b4a2
Ytka2235 <117965265+Ytka2235@users.noreply.github.com>
2023-05-20 14:04:19 +0300

    Add files via upload

---
+++ Swaper/Swaper/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Swaper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox = new System.Windows.Forms.CheckedListBox();
+            this.but_swp = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox
+            // 
+            this.checkedListBox.CheckOnClick = true;
+            this.checkedListBox.FormattingEnabled = true;
+            this.checkedListBox.Items.AddRange(new object[] {
+            "Аршин",
+            "Локоть",
+            "Косая сажень",
+            "Маховая сажень",
+            "Ладонь",
+            "Пядь",
+            "Шаг",
+            "Верста",
+            "Вершок"});
+            this.checkedListBox.Location = new System.Drawing.Point(36, 75);
+            this.checkedListBox.Name = "checkedListBox";
+            this.checkedListBox.Size = new System.Drawing.Size(143, 139);
+            this.checkedListBox.TabIndex = 0;
+            this.checkedListBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox_ItemCheck);
+            // 
+            // but_swp
+            // 
+            this.but_swp.Location = new System.Drawing.Point(208, 33);
+            this.but_swp.Name = "but_swp";
+            this.but_swp.Size = new System.Drawing.Size(75, 23);
+            this.but_swp.TabIndex = 1;
+            this.but_swp.Text = "Swap";
+            this.but_swp.UseVisualStyleBackColor = true;
+            this.but_swp.Click += new System.EventHandler(this.but_swp_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(316, 33);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 13);
+            this.label.TabIndex = 2;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(36, 33);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(143, 20);
+            this.textBox.TabIndex = 3;
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Location = new System.Drawing.Point(208, 87);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(126, 17);
+            this.checkBox.TabIndex = 4;
+            this.checkBox.Text = "Записывать в файл";
+            this.checkBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(476, 236);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.but_swp);
+            this.Controls.Add(this.checkedListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox;
+        private System.Windows.Forms.Button but_swp;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.CheckBox checkBox;
+    }
+}
+
---
+++ Swaper/Swaper/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Swaper
+{
+    public partial class Form1 : Form
+    {
+        double[] coff = new double[] {0.71,0.45,2.48,1.78,0.1016,0.1778,0.71,1066.8,0.0444};
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            for (int i = 0; i < checkedListBox.Items.Count; ++i)
+                if (i != e.Index) checkedListBox.SetItemChecked(i, false);
+        }
+
+        private async void but_swp_Click(object sender, EventArgs e)
+        {
+            double number;
+            if (!double.TryParse(textBox.Text, out number)) return;
+            double x = Convert.ToDouble(textBox.Text);
+
+            int y = -1;
+            foreach (object itemChecked in checkedListBox.CheckedItems)
+            {
+                if (checkedListBox.GetItemCheckState(checkedListBox.Items.IndexOf(itemChecked)) == CheckState.Checked)
+                    y = checkedListBox.Items.IndexOf(itemChecked);
+            }
+            if (y == -1) return;
+            label.Text = Convert.ToString(x * coff[y]);
+
+            if (checkBox.Checked)
+            {
+                string path = "C:\\Users\\admin_0\\source\\repos\\Swaper\\SWAP.txt";
+                using (StreamWriter writer = new StreamWriter(path, true))
+                {
+                    await writer.WriteLineAsync($"{textBox.Text} {checkedListBox.GetItemText(checkedListBox.Items[y])} = {label.Text} метр");
+                }
+            }
+
+        }
+    }
+}
